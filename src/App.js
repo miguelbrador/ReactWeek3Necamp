812669a20a3ff9b5c3c8c30cd267ecc833fc96d7
miguelbrador55 <miguelbrador55@gmail.com>
2021-02-03 13:01:30 -0500

    react week3Nucamp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/Maincomponent';
 import './App.css';
 
 class App extends Component {
+
     render() {
         return (
             <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
+                <Main />
             </div>
         );
     }
